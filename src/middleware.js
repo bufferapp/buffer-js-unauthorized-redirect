0189a0890181f88beccb4dfdc4e44c2d5bad2961
Harrison Harnisch <hharnisc@gmail.com>
2018-10-18 17:59:36 -0500

    initial commit

---
+++ src/middleware.js
@@ -0,0 +1,22 @@
+// TODO: async data fetch needs to be transpiled (breaks babel)
+import { actionTypes } from '@bufferapp/async-data-fetch'
+import { logoutUrl } from '@bufferapp/session-manager'
+
+export default ({ getState }) => next => action => {
+  next(action)
+  if (
+    action.type.endsWith(actionTypes.FETCH_FAIL) &&
+    action.error === 'Unauthorized'
+  ) {
+    const {
+      environment: { environment },
+    } = getState()
+    // redirect to logout url
+    // this will clean up cookies and show the login screen
+    window.location.replace(
+      logoutUrl({
+        production: environment === 'production',
+      }),
+    )
+  }
+}
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+import { middleware } from '../src/index'
+
+describe('middleware', () => {
+  it('should export middleware', () => {
+    expect(middleware).toBeDefined()
+  })
+})
